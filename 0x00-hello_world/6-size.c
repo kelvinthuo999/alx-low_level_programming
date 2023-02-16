502dd8348da74cdb4c762ffff26f5cb18c49efab
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-02-15 23:47:12 -0800

    sizeof correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	char a;
 	int x;
-	long int xy;
-	long long int xyz;
+	long xy;
+	long long xyz;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n",sizeof(a));
-	printf("Size of an int: %d byte(s)\n",sizeof(x));
-	printf("Size of a long int: %d byte(s)\n",sizeof(xy));
-	printf("Size of a long long int: %d byte(s)\n",sizeof(xyz));
-	printf("Size of a float: %d byte(s)",sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(x));
+	printf("Size of a long int: %d byte(s)\n", sizeof(xy));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(xyz));
+	printf("Size of a float: %d byte(s)", sizeof(f));
 	return (0);
 }
