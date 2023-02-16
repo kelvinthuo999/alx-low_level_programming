a4c21d6ccd17c097951a5672c3ae4685b2a717c6
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-02-15 22:51:09 -0800

    betty correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: 0 on success
+ * Return: Always 0(success)
  */
 
 int main(void)
@@ -10,7 +10,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-/**
- * Return: 0 if program is successful
- */
+
 
