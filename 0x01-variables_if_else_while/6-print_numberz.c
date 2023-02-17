fb8455ed2524363a20ac208621537bca688475b5
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-02-17 05:05:12 -0800

    print numbers using the putchar func

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: (0)success
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
