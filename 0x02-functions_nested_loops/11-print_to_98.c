5c9d0df40e65d07a4812cc6bcb40a9b88a75b970
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-02-21 07:16:39 -0800

    write to a particular number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,18 @@
  */
 
 void print_to_98(int n)
-
 {
-
 	while (n < 98)
-
 	{
-
 		printf("%i, ", n);
-
 		n++;
-
 	}
-
-
 
 	while (n > 98)
-
 	{
-
 		printf("%i, ", n);
-
 		n--;
-
 	}
-
 	printf("98");
-
 	putchar('\n');
-
 }
