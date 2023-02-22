da78f800c2bf48f629d866d5f8c061785d5957a1
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-02-21 23:56:15 -0800

    check if zero is positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check if a number is positive
+ *
+ * @i: integer to be checked
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
