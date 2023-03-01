399513e65d04790df6bf00de8160b312c159ee9e
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-03-01 10:20:08 -0800

    encode a string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @ch: pointer to character
+ * Return: char
+ */
+
+char *rot13(char *ch)
+{
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (ch[indx1])
+	{
+		for (indx2 = 0; indx2 < 52; indx2++)
+		{
+			if (ch[indx1] == alphabet[indx2])
+			{
+				ch[indx1] = rot13key[indx2];
+				break;
+			}
+		}
+
+		indx1++;
+	}
+
+	return (ch);
+}
