306dd6dbf912dd925dd91d21a9d4813d5c9c9189
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-03-10 00:52:02 -0800

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: variable containing number of arguments
+ * @argv: character variable holding the string values of argc
+ * @Return: 0 on success
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
