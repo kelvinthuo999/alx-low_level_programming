e93437eabb0109f07af9e3b3d9ec8ea42350609a
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-03-10 01:01:34 -0800

    int value of argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: variable containing the number of arguments
+ * @argv: character variable containing the string values of argc
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
