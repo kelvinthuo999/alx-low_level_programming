b6db7e32d35a932caef4746358555918369c514d
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-05-22 22:10:39 -0700

    check if a letter is uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check if a letter is uppercase
+ * @c: letter to be checked
+ * Return: 1(success), otherwise 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
