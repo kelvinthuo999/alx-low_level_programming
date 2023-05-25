f1bc3ec280c9eee686b618269a679c3e869a9bb2
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-05-25 11:06:56 -0700

    fizzbuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz test
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	/* variables declaration */
+	int num;
+
+	/* using a combination of if statements and loops to complete task */
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
+	return (0);
+}
