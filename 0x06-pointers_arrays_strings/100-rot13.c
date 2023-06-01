ee09e8361482466bfb9655b2138549fab38b13c6
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-01 07:44:44 -0700

    encode a string

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,30 +7,24 @@
  */
 char *rot13(char *s)
 {
-	int index1, index2;
+	int index1;
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	/* Iterate through each character in the string */
 	for (index1 = 0; s[index1] != '\0'; index1++)
 	{
-		/* Check if the character is alphabetical */
-		if ((s[index1] >= 'a' && s[index1] <= 'z')
-				|| (s[index1] >= 'A' && s[index1] <= 'Z'))
+		int index2 = 0;
+		while (a[index2] != '\0')
 		{
-			/* Iterate through the rot13 encoding arrays */
-			for (index2 = 0; index2 < 52; index2++)
+			if (s[index1] == a[index2])
 			{
-				/* Match the character to its corresponding rot13 encoded character */
-				if (s[index1] == a[index2])
-				{
-					/* Replace the character with its rot13 encoded counterpart */
-					s[index1] = b[index2];
-					break;
-				}
+				s[index1] = b[index2];
+				break;
 			}
+			index2++;
 		}
 	}
 
-	return (s);  /* Return the encoded string */
+	return s;
 }
+
