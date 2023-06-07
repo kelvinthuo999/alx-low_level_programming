6f8a5bf1452e0cea8bc5b7e83b4b6bf2b713c71c
KelvinThuo <123747670+kelvinthuo999@users.noreply.github.com>
2023-06-07 14:39:38 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,51 +1,37 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Calculates the square root of a number.
- * @n: The number.
- * @start: integer to point to the start
- * @end: integer pointing to the end
- *
- * Return: The square root of the number, or -1 if no square root exists.
+ * _sqrt_recursion - computes the square root of a number
+ * @n: the number to compute the square root of
+ * Return: the square root of n, or -1 if it does not have a natural square root
  */
-
-int sqrt_helper(int n, int start, int end);
 int _sqrt_recursion(int n)
 {
-	/* Check if the number is negative */
-	if (n < 0)
-		return (-1);
+    if (n < 0)
+        return -1;
 
-	/* Call the helper function to calculate the square root */
-	return (sqrt_helper(n, 0, n));
+    return sqrt_helper(n, 0, n);
 }
 
 /**
- * sqrt_helper - Recursive helper function to calculate the square root.
- * @n: The number.
- * @start: integer to point to the start
- * @end: integer to point to the end
- *
- * Return: The square root of the number, or -1 if no square root exists.
+ * sqrt_helper - helper function for computing the square root recursively
+ * @n: the number to compute the square root of
+ * @start: the starting point for the search
+ * @end: the ending point for the search
+ * Return: the square root of n, or -1 if it does not have a natural square root
  */
-
 int sqrt_helper(int n, int start, int end)
 {
-	/* variable declaration and initialization */
-	int mid = (start + end) / 2;
-	int square = mid * mid;
+    if (start > end)
+        return -1;
 
-	/* Base case: square root does not exist */
-	if (start > end)
-		return (-1);
-	/* Base case: square root found */
-	if (square == n)
-		return (mid);
+    int mid = start + (end - start) / 2;
+    int square = mid * mid;
 
-	/* Continue searching in the upper half */
-	if (square < n)
-		return (sqrt_helper(n, mid + 1, end));
-
-	/* Continue searching in the lower half */
-	return (sqrt_helper(n, start, mid - 1));
+    if (square == n)
+        return mid;
+    else if (square < n)
+        return sqrt_helper(n, mid + 1, end);
+    else
+        return sqrt_helper(n, start, mid - 1);
 }
