14e17ce1969a1024572f0928669efbe31f54a599
KelvinThuo <123747670+kelvinthuo999@users.noreply.github.com>
2023-06-07 14:45:00 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,10 @@ int _sqrt_recursion(int n)
     if (n < 0)
         return -1;
 
-    return sqrt_helper(n, 0, n);
+    if (n == 0 || n == 1)
+        return n;
+
+    return sqrt_helper(n, 1, n);
 }
 
 /**
@@ -24,14 +27,16 @@ int _sqrt_recursion(int n)
 int sqrt_helper(int n, int start, int end)
 {
     int mid = start + (end - start) / 2;
-    int square = mid * mid;
+    int quotient = n / mid;
+    int diff = quotient - mid;
+    
     if (start > end)
         return -1;
-    
-    if (square == n)
+
+    if (diff == 0)
         return mid;
-    else if (square < n)
-        return sqrt_helper(n, mid + 1, end);
-    else
+    else if (diff < 0)
         return sqrt_helper(n, start, mid - 1);
+    else
+        return sqrt_helper(n, mid + 1, end);
 }
