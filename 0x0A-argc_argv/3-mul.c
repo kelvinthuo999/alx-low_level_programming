c5374f72eca54b154f930d7178841b4830705a28
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-09 02:59:03 -0700

    multiply two numbers received in the command line

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * multiply - Calculates the product of two numbers.
- * @num1: The first number.
- * @num2: The second number.
- *
- * Return: The product of num1 and num2.
- */
-int multiply(int num1, int num2)
-{
-	return (num1 * num2);
-}
+int multiply(int num1, int num2);
 
 /**
- * main - Prints the multiplication of two numbers, followed by a new line.
+ * main - Entry point of the program.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -23,25 +13,34 @@ int multiply(int num1, int num2)
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, prod;
+	int num1, num2, result;
 
-	/* Check if the program received the correct number of arguments */
 	if (argc != 3)
 	{
-		printf("Error \n");
+		printf("Error: Two arguments are required.\n");
 		return (1);
 	}
-	else
-	{
-		/* Convert the arguments from strings to integers */
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
 
-		/* Calculate the product using the multiply function */
-		prod = multiply(num1, num2);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = multiply(num1, num2);
 
-		/* Print the product */
-		printf("%d\n", prod);
-	}
+	printf("Product: %d\n", result);
+
 	return (0);
+}
+
+/**
+ * multiply - Multiplies two numbers.
+ * @num1: The first number.
+ * @num2: The second number.
+ *
+ * Return: The product of the two numbers.
+ */
+int multiply(int num1, int num2)
+{
+	int result;
+
+	result = num1 * num2;
+	return (result);
 }
