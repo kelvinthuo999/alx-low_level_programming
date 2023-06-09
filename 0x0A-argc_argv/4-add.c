97032a432b28c2273255a95f9e4fe4ce991eeb29
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-09 04:48:37 -0700

    add number passed in the command line

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,33 +2,42 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    int i, j;
+	/* Initialize variables */
+	int sum = 0;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (argc == 1)
+	{
+		/* Outputting the sum when no arguments are provided */
+		printf("0\n");
+		return (0);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        /* Check if each character in the argument is a digit */
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        
-        /* Convert the argument to an integer and add it to the sum */
-        sum += atoi(argv[i]);
-    }
+	for (int i = 1; i < argc; i++)
+	{
+		/* Checking if each character in the argument is a digit */
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				/* Printing an error message if a non-digit character is found */
+				printf("Error\n");
+				return (1);
+			}
+		}
+		/* Converting the argument to an integer and adding it to the sum */
+		sum += atoi(argv[i]);
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	/* Outputting the sum */
+	printf("%d\n", sum);
+	return (0);
 }
