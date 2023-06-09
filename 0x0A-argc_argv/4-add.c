1e33dd9875305203b7295e7c50b52929eec8167d
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-09 03:22:24 -0700

    add numbers in the command line

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Description: Adds all positive numbers and prints the sum.
+ * Return: (0) for success, (1) for error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	int error = 0;
+
+	/* Loop through the arguments */
+	for (i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		/* Check if the number is positive */
+		if (num <= 0)
+		{
+			error = 1;
+			break;
+		}
+
+		sum += num;
+	}
+
+	/* Check if any non-positive number was detected */
+	if (error)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* Print the sum of positive numbers */
+	printf("%d\n", sum);
+	return (0);
+}
