b43c25bac95fc3f9165c5462ef48ff47e9e00e5b
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-19 06:08:49 -0700

    prints values stored in a struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Print the values stored in a struct
+ * @d: struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if ((d->name) != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)");
+		}
+		
+		printf("Age: %f\n", d->age);
+		
+		if ((d->owner) != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)");
+		}
+	}
+}
