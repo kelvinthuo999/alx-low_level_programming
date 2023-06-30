c3b26cb217fcdb0b58a14f238c0b0fbb0e08f968
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-30 06:07:27 -0700

    function to free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * free_list - frees a list
+ * @head: pointer to a list to be freed
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	/* variable declaration */
+	list_t *tmp;
+	list_t *current = head;
+
+	/* while loop to traverse the list */
+	tmp = head;
+	while (tmp != NULL)
+	{
+		next = current->next;
+		free(current->next);
+		free(current);
+		current = next;
+	}
+}
