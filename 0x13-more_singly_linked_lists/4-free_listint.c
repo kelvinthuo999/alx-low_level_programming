d43c66fee443dc5f717df7c439447be96be3cfac
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-07-03 03:56:01 -0700

    free memory allocated to list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees memory that has been allocated to a list
+ * @head: pointer to the start of a list
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	/* variable declaration */
+	listint_t *current;
+
+	/* check if the list is empty */
+	if (head == NULL)
+		return;
+
+	while(head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+	free(head);
+}
